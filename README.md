21eb7aebaf19259e9b4653dfa8cb9029b10fc465
Gustavo Scavazza <ggscavazza.java@gmail.com>
2025-02-11 17:52:57 -0300

    Fix: ajustando README

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
-# ccb_app_visitas
+# APP Visitas CCB
+Aplicativo para controle de visitas à enfermos e demais irmãos da CCB. Criado voluntariamente e gratuitamente.
 
-A new Flutter project.
+## Idealizador
+Gustavo G. Scavazza
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Programador
+Gustavo G. Scavazza
--- lib/shareds/app_theme.dart
+++ lib/shareds/app_theme.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData lightTheme = ThemeData(
-    primarySwatch: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
+  // Definição das cores Flat UI Colors China
+  static const Color primaryColor = Color(0xFF2f3542); // Prestige Blue
+  static const Color accentColor = Color(0xFFff4757); // Watermelon
+  static const Color backgroundColor = Color(0xFF2f3542); // Prestige Blue
+  static const Color cardColor = Color(0xFF1e90ff); // Clear Chill
+  static const Color textColor = Colors.white;
+
+  // Definição do tema escuro
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: primaryColor,
+    scaffoldBackgroundColor: backgroundColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: primaryColor,
+      foregroundColor: textColor,
+    ),
     textTheme: const TextTheme(
-      headlineMedium: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+      headlineMedium: TextStyle(
+          fontSize: 22, fontWeight: FontWeight.bold, color: textColor),
+      bodyMedium: TextStyle(fontSize: 16, color: textColor),
+    ),
+    cardColor: cardColor,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: WidgetStatePropertyAll(accentColor),
+        foregroundColor: WidgetStatePropertyAll(textColor),
+      ),
     ),
   );
 }
